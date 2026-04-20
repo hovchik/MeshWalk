7c55845dfcc9323d911c68a92e176e7739f1a3ed
hovch <hovchik@gmail.com>
2026-04-20 22:55:46 +0400

    commit.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.meshwalk.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.4"
     }
 
     buildTypes {
